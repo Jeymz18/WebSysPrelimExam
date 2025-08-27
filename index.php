fb9af1461b8e2b261c6f92f18e017d8c7febf023
Jeymz18 <ayuntingjames12@gmail.com>
2025-08-27 02:15:10 +0000

    Ayunting, Prelim Lab Exam

--- index.php
+++ index.php
@@ -45,8 +45,8 @@
                 if ($error) {
                     echo "<p class='result' style='color: red;'>$error</p>";
                 } else {
-                    echo "<p class='result'>Result: $num1 $operation $num2 $operation $num3  + $average = $result</p>";
-                }
+                    echo "<p class='result'>Result: $num1 $operation $num2 $operation $num3 = $result</p>";
+                    echo "<p class='average'>Average: $result </p>";
             }
         ?>
     </div>
